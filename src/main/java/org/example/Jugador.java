433445879c234b513b160f3f402fa7239974d407
206 <206@academicos.cesde.edu.co>
2022-11-21 12:07:02 -0500

    se crean variables

--- src/main/java/org/example/Jugador.java
+++ src/main/java/org/example/Jugador.java
@@ -2,11 +2,22 @@ package org.example;
 
 public class Jugador {
 
-    String nombre="James";
-    int edad;
-    double apellidos;
+    String nombre, apellidos, posicion, equipo;
+    int edad, numeroDeCamisa ;
 
-    public Jugador(String nombre) {
+
+
+    public Jugador() {
+    }
+
+    public Jugador(String nombre, String apellidos, String posicion, String equipo, int edad, int numeroDeCamisa) {
         this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.posicion = posicion;
+        this.equipo = equipo;
+        this.edad = edad;
+        this.numeroDeCamisa = numeroDeCamisa;
     }
 }
+
+
